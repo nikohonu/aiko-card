c67977ecb8a5596e95d02875ad69292ee6bab423
Niko Honu <nikohonu@gmail.com>
2023-03-04 13:10:18 +0200

    add text translation

--- aiko_card/__main__.py
+++ aiko_card/__main__.py
@@ -1,17 +1,33 @@
+import click
 import pytesseract
+from translatepy import Translator
+from translatepy.translators.google import GoogleTranslateV2
 
 from aiko_card.formatting import clear_text
 from aiko_card.screenshot import make_screenshot
 
 
-def main():
+@click.command()
+@click.option(
+    "-d",
+    "--dest",
+    help="Language into which the sentence will be translated",
+    required=True,
+)
+def aiko_card(dest):
+    translator = GoogleTranslateV2()
     screenshot = make_screenshot()
     screenshot_region = make_screenshot(region=True)
     screenshot_region = screenshot_region.resize(
         (screenshot_region.width * 2, screenshot_region.height * 2)
     )
     sentence = clear_text(pytesseract.image_to_string(screenshot_region))
-    print(sentence)
+    translation = translator.translate(sentence, dest)
+    print(sentence, translation)
+
+
+def main():
+    aiko_card()
 
 
 if __name__ == "__main__":
